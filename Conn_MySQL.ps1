e054f7cba69b27552ba58376367848e81ca8c88e
menpengpeng <menpengpeng@163.com>
2017-11-17 10:48:28 +0800

    powershell connection mysql V0.1

---
+++ Conn_MySQL.ps1
@@ -0,0 +1,8 @@
+﻿Clear-Host
+set-ExecutionPolicy RemoteSigned  #允许脚本运行
+Import-Module MySqlCmdlets
+[string] $PasswordFlie = 'D:\serveinfo\file.txt'
+[string] $username ="root"
+$password = ConvertTo-SecureString -String "123qwe!@#" -AsPlainText -Force
+Connect-MySqlServer -Server 192.168.2.223 -UserName $username -Password $password
+Write-Host "Mysql connctiond successd"
